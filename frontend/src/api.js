c5ee91bc4b3390fe92686d4c03e41bcdfa072752
Navadeep <navadeepvalmiki20@gmail.com>
2026-05-29 10:06:23 +0530

    Connect to render engine

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:8081/api/v1',
+    baseURL: 'https://smartbook-marketplace.onrender.com',
     headers: {
         'Content-Type': 'application/json',
     }
