56431188a7a71aafaba92b33e7af393ebb81ca22
Navadeep Valmiki <navadeepvalmiki20@gmail.com>
2026-07-30 18:01:16 +0530

    Update AppointmentSystemApplication.java

--- appointment-system/src/main/java/com/smartbook/appointment_system/AppointmentSystemApplication.java
+++ appointment-system/src/main/java/com/smartbook/appointment_system/AppointmentSystemApplication.java
@@ -1,15 +1,36 @@
 package com.smartbook.appointment_system;
 
+import com.smartbook.appointment_system.model.Role;
+import com.smartbook.appointment_system.model.RoleName;
+import com.smartbook.appointment_system.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableAsync
 public class AppointmentSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppointmentSystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AppointmentSystemApplication.class, args);
+    }
 
+    @Bean
+    CommandLineRunner initRoles(RoleRepository roleRepository) {
+        return args -> {
+            if (roleRepository.count() == 0) {
+                Role userRole = new Role();
+                userRole.setName(RoleName.ROLE_USER);
+                roleRepository.save(userRole);
+
+                Role providerRole = new Role();
+                providerRole.setName(RoleName.ROLE_PROVIDER);
+                roleRepository.save(providerRole);
+
+                Role adminRole = new Role();
+                adminRole.setName(RoleName.ROLE_ADMIN);
+                roleRepository.save(adminRole);
+            }
+        };
+    }
 }
